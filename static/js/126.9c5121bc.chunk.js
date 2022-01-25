542b2f3d18bb23c3c23bf534f98977299e1222dd
Andrei Potapov <andr3008@gmail.com>
2022-01-25 17:53:21 +0200

    Updates

---
+++ static/js/126.9c5121bc.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[126],{126:function(e,n,t){t.r(n),t.d(n,{default:function(){return r}});var u=t(184);function r(){return(0,u.jsx)("h2",{children:"Page not found"})}}}]);
+//# sourceMappingURL=126.9c5121bc.chunk.js.map
